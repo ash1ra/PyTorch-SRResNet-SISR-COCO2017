3bb51023f3c5c9a699168dddf52d1e358204f618
ash1ra <ash1rawtf@gmail.com>
2025-10-15 12:34:19 +0300

    feat: use `v2` transforms from torchvision

--- utils.py
+++ utils.py
@@ -1,24 +1,34 @@
 from pathlib import Path
 
+import torch
 from torch import Tensor
-from torchvision import transforms
 from torchvision.io import decode_image
+from torchvision.transforms import InterpolationMode
+from torchvision.transforms import v2 as transforms
 
 
 def transform_image(
     img_path: Path, scaling_factor: int, crop_size: int
 ) -> tuple[Tensor, Tensor]:
-    img_tensor = decode_image(img_path.__fspath__()) / 255.0 * 2.0 - 1.0
+    img_tensor = decode_image(img_path.__fspath__())
 
-    hr_transform = transforms.RandomCrop(size=(crop_size, crop_size))
+    hr_transform = transforms.Compose(
+        [
+            transforms.RandomCrop(size=(crop_size, crop_size)),
+            transforms.ToDtype(torch.float32, scale=True),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ]
+    )
     lr_transform = transforms.Compose(
         [
             transforms.CenterCrop(size=(crop_size, crop_size)),
             transforms.Resize(
                 size=(crop_size // scaling_factor, crop_size // scaling_factor),
-                interpolation=transforms.InterpolationMode.BICUBIC,
+                interpolation=InterpolationMode.BICUBIC,
                 antialias=True,
             ),
+            transforms.ToDtype(torch.float32, scale=True),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
         ]
     )
 
