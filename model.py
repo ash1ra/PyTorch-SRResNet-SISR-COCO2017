feat: normalize `img_tensor` to the range of `[-1, 1]` (for tanh activation); fix small bugs; implement Mixed Precision Training technique; add `pim_memory=True` and `NUM_WORKERS=8`; add tqdm library

model.py
@@ -102,9 +102,10 @@ class SRResNet(nn.Module):
             activation="prelu",
         )
 
-        self.res_blocks = [
-            ResBlock(n_channels=n_channels, kernel_size=small_kernel_size)
-        ] * n_res_blocks
+        self.res_blocks = nn.Sequential(
+            *[ResBlock(n_channels=n_channels, kernel_size=small_kernel_size)]
+            * n_res_blocks
+        )
 
         self.conv_block2 = ConvBlock(
             in_channels=n_channels,
@@ -113,13 +114,16 @@ class SRResNet(nn.Module):
             norm_layer=True,
         )
 
-        self.subpixel_conv_blocks = [
-            SubPixelConvBlock(
-                n_channels=n_channels,
-                kernel_size=small_kernel_size,
-                scaling_factor=2,
-            )
-        ] * int(math.log2(scaling_factor))
+        self.subpixel_conv_blocks = nn.Sequential(
+            *[
+                SubPixelConvBlock(
+                    n_channels=n_channels,
+                    kernel_size=small_kernel_size,
+                    scaling_factor=2,
+                )
+            ]
+            * int(math.log2(scaling_factor))
+        )
 
         self.conv_block3 = ConvBlock(
             in_channels=n_channels,

train.py
@@ -2,14 +2,26 @@ from typing import Literal
 
 import torch
 from torch import nn, optim
+from torch.amp import GradScaler, autocast
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from data_processing import SRDataset
 from model import SRResNet
 
-BATCH_SIZE = 16
+SCALING_FACTOR = 4
+CROP_SIZE = 96
+
+N_CHANNELS = 64
+N_RES_BLOCKS = 16
+LARGE_KERNEL_SIZE = 9
+SMALL_KERNEL_SIZE = 3
+
+BATCH_SIZE = 32
 LEARNING_RATE = 1e-4
-EPOCHS = 3
+EPOCHS = 10
+
+NUM_WORKERS = 8
 
 
 def train_step(
@@ -17,24 +29,32 @@ def train_step(
     model: nn.Module,
     loss_fn: nn.Module,
     optimizer: optim.Optimizer,
+    scaler: GradScaler,
     device: Literal["cpu", "cuda"] = "cpu",
 ) -> float:
     train_loss = 0
     model.train()
 
-    for i, (hr_image_tensor, lr_image_tensor) in enumerate(data_loader):
-        hr_image_tensor = hr_image_tensor.to(device)
-        lr_image_tensor = lr_image_tensor.to(device)
+    for i, (hr_image_tensor, lr_image_tensor) in enumerate(
+        tqdm(data_loader, desc="Training", leave=False)
+    ):
+        hr_image_tensor = hr_image_tensor.to(device, non_blocking=True)
+        lr_image_tensor = lr_image_tensor.to(device, non_blocking=True)
 
-        preds = model(lr_image_tensor)
-
-        loss = loss_fn(preds, hr_image_tensor)
+        with autocast(device):
+            preds = model(lr_image_tensor)
+            loss = loss_fn(preds, hr_image_tensor)
 
         train_loss += loss.item()
 
         optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        # loss.backward()
+        # optimizer.step()
 
     train_loss /= len(data_loader)
 
@@ -49,26 +69,41 @@ def train(
     epochs: int,
     device: Literal["cpu", "cuda"] = "cpu",
 ) -> None:
-    for epoch in range(1, epochs + 1):
-        train_loss = train_step(data_loader, model, loss_fn, optimizer, device)
-        print(f"Epoch: {epoch} | Train loss: {train_loss}")
+    scaler = GradScaler(device)
+
+    progress_bar = tqdm(range(1, epochs + 1), desc="Epochs", leave=True)
+
+    for epoch in progress_bar:
+        train_loss = train_step(data_loader, model, loss_fn, optimizer, scaler, device)
+        progress_bar.set_postfix({"loss": f"{train_loss:.4f}"})
+        print()
+        # print(f"Epoch: {epoch} | Train loss: {train_loss:.4f}")
 
 
 def main() -> None:
-    # device = "cuda" if torch.cuda.is_available() else "cpu"
-    device = "cpu"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     dataset = SRDataset(
-        data_folder="data/COCO2017_train", scaling_factor=4, min_size=256
+        data_folder="data/COCO2017_train",
+        scaling_factor=SCALING_FACTOR,
+        crop_size=CROP_SIZE,
+        # dev_mode=True,
     )
-    data_loader = DataLoader(dataset=dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    data_loader = DataLoader(
+        dataset=dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+        pin_memory=True if device == "cuda" else False,
+        num_workers=NUM_WORKERS,
+    )
 
     model = SRResNet(
-        n_channels=64,
-        large_kernel_size=9,
-        small_kernel_size=3,
-        n_res_blocks=16,
-        scaling_factor=4,
+        n_channels=N_CHANNELS,
+        large_kernel_size=LARGE_KERNEL_SIZE,
+        small_kernel_size=SMALL_KERNEL_SIZE,
+        n_res_blocks=N_RES_BLOCKS,
+        scaling_factor=SCALING_FACTOR,
     ).to(device)
 
     loss_fn = nn.MSELoss()