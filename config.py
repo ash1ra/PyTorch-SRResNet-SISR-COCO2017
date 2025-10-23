ed7d5fbd661fc8e1ad3a1ac5387ffce10b285697
ash1ra <ash1rawtf@gmail.com>
2025-10-23 12:33:47 +0300

    feat: add `RotatingFileHandler` to the logger

--- config.py
+++ config.py
@@ -1,19 +1,43 @@
 import logging
 import sys
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+from datetime import datetime
 
 
-def create_logger(log_level: str) -> logging.Logger:
+def create_logger(
+    log_level: str,
+    log_file: str | None = None,
+    max_log_size: int = 5 * 1024 * 1024,
+    backup_count: int = 10,
+) -> logging.Logger:
     logger = logging.getLogger(__name__)
-    logger.setLevel(getattr(logging, log_level.upper()))
+    logger.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter(
-        "%(asctime)s | %(levelname)s | %(message)s", datefmt="%H:%M:%S"
+        "%(asctime)s | %(levelname)s | %(message)s", datefmt="%Y.%m.%d %H:%M:%S"
     )
 
     logger.handlers.clear()
 
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(formatter)
+    console_handler.setLevel(getattr(logging, log_level.upper()))
     logger.addHandler(console_handler)
+
+    if not log_file:
+        current_date = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        log_file = f"logs/srresnet_{current_date}.log"
+
+    log_file_path = Path(log_file)
+    log_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    file_handler = RotatingFileHandler(
+        filename=log_file_path,
+        maxBytes=max_log_size,
+        backupCount=backup_count,
+    )
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
     return logger
